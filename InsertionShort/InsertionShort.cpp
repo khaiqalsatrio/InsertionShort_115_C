743a7c081fa597103202c81c176eab82b6ba79c1
khaiqalsatrio <khaiqalsatrio29@gmail.com>
2023-03-23 15:25:34 +0700

    procedure insertionshort

--- InsertionShort/InsertionShort.cpp
+++ InsertionShort/InsertionShort.cpp
@@ -5,7 +5,7 @@ int arr[20];      // Membuat array dengan panjang data 20
                   // Membuat variable inputan n              
 int n;
 
-void input ()
+void input()
 {    // Procedure input
 	while (true)
 	{
@@ -22,10 +22,39 @@ void input ()
 		{
 			cout << "\nArray yang anda masukan maksimal 20 elemen. \n";     // Menampilkan pesan jika data
 		}
+	}
+	cout << endl;												// Membuat jarak per baris program
+	cout << "========================" << endl;						// Membuat tampilan data element Array 
+	cout << "Masukan Element Array " << endl;
+	cout << "========================" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "data ke-n" << (i + 1) << ": ";
+		cin >> arr[i];
+	}
+}
+// procedure insertionshort
+void insertionshort()
+}
+int temp//membuat variable daya temporer atau penyimpanan sementara
+int j; // membuat variable j sebagai panda
+
+for (int i = 1; i < n; i++) // membuat looping dengan i di mulai dari 1 hingga n-1
+{
+	temp = arr[i];
+	j = i - 1;
+
+	while (j >= 0 && arr[j] > temp)
+	{
+		arr[j + 1] = arr[j];
+		j--;
     }
-	cout << endl;											// Membuat jarak per baris program
-	cout << "Masukan Element Array " << endl;					// Membuat tampilan data element Array 
-	cout << "======================" << endl;
+
+	arr[j + 1] temp;
+
+	cout << endl;
+	cout << 
 
     
 	
