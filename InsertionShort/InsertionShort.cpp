beee2dff0618ac33eb9061c7d8e6f43b6d9919b2
khaiqalsatrio <khaiqalsatrio29@gmail.com>
2023-03-23 14:59:44 +0700

    membuat variable inputan n

--- InsertionShort/InsertionShort.cpp
+++ InsertionShort/InsertionShort.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
+using namespace std;
 
+int arr[20];
+int n;
+
+void input ()
+{    // Procedure input
+	while (true)
+	{
+		cout << "Masukan jumlah data pada array : ";
+		cin >> n;
+
+		if (n <= 20)
+		{
